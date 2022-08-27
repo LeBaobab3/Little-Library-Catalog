b900c90341437295bc85dd5a3c9336c0882d6993
Leila A <97766126+LeBaobab3@users.noreply.github.com>
2022-08-27 15:16:14 -0500

    mongoose connection attempt

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const dotenv = require("dotenv");
 const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
@@ -10,6 +11,18 @@ app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
+
+const mongoose = require("mongoose");
+const connectDB = async () => {
+  try {
+    //!Change: Additional properties not required
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+};
 
 app.use("/", indexRouter);
 
